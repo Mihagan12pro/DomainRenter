110b554513fe88834d1339fbe32ca0da3d98bf15
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-05-09 17:46:06 +0300

    Add project files.

---
+++ src/DomainModels/Domains/RentedDomain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Domains
+{
+    public class RentedDomain
+    {
+        public Guid Id { get; }
+
+        public required Guid DomainId { get; set; }
+
+        [Column("from")]
+        public required DateOnly StartOfRenting { get; set; }
+
+        [Column("to")]
+        public required DateOnly EndOfRenting { get; set; }
+    }
+}
