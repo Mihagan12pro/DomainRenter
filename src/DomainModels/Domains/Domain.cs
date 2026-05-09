5ef863be006903fbd1985fb15bbc6728aa851c56
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-05-09 17:46:06 +0300

    Add project files.

---
+++ src/DomainModels/Domains/Domain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Domains
+{
+    public class Domain
+    {
+        public Guid Id { get; }
+
+        public required string Name { get; set; }
+    }
+}
