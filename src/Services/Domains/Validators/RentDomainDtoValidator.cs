84dcd48636a90c00392b9062be433671a6b614dd
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-05-30 15:15:43 +0300

    feat: implement domain validation

---
+++ src/Services/Domains/Validators/RentDomainDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using Contracts.Domains;
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace Services.Domains.Validators
+{
+    public class RentDomainDtoValidator : AbstractValidator<RentDomainDto>
+    {
+        public RentDomainDtoValidator()
+        {
+            DateTime now = DateTime.Now;
+
+            RuleFor(dto => dto.DomainName.Contains('.'))
+                .NotEqual(false)
+                .WithMessage("Domain name can't be without any dots!");
+
+            RuleFor(dto => Uri.CheckHostName(dto.DomainName))
+                .Equal(UriHostNameType.Dns)
+                .WithMessage("Invalid domain name!");
+
+            RuleFor(dto => dto.EndRentDate.CompareTo(new DateOnly(now.Year, now.Month, now.Day)))
+                .Equal(1)
+                .WithMessage("Too late end rate!");
+        }
+    }
+}
